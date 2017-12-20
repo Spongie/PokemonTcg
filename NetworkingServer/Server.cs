19a27f2f67048786e5ce53c3404a0e1a6c8a939a
Philip Persson <kalllez123@hotmail.se>
2017-12-21 00:11:34 +0100

    Test

--- NetworkingServer/Server.cs
+++ NetworkingServer/Server.cs
@@ -1,12 +1,8 @@
 ﻿using NetworkingClient.Common;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NetworkingServer
 {
