84188575850a78c0a4aece972d6abb6f93fc042a
Philip Persson <kalllez123@hotmail.se>
2018-01-01 02:13:16 +0100

    Coinflipper

---
+++ TCGCards/Core/CoinFlipper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TCGCards.Core
+{
+    public static class CoinFlipper
+    {
+        private static Random random = new Random();
+
+        public static bool FlipCoin()
+        {
+            return random.Next(2) > 0;
+        }
+
+        public static int FlipCoins(int count)
+        {
+            int heads = 0;
+
+            for(int _ = 0; _ < count; _++)
+            {
+                heads += random.Next(2);
+            }
+
+            return heads;
+        }
+    }
+}
