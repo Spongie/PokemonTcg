9d534d3eb0680ff76ba987882298cd6bcbf2f97a
Philip Persson <kalllez123@hotmail.se>
2018-01-13 21:29:16 +0100

    Costs for abra attacks

--- TCGCards/PokemonCards/TeamRocket/Attacks/Psyshock.cs
+++ TCGCards/PokemonCards/TeamRocket/Attacks/Psyshock.cs
@@ -1,9 +1,20 @@
-﻿using TCGCards.Core;
+﻿using System.Collections.Generic;
+using TCGCards.Core;
 
 namespace TCGCards.PokemonCards.TeamRocket.Attacks
 {
     public class Psyshock : Attack
     {
+        public Psyshock()
+        {
+            Name = "Psyshock";
+            Description = string.Empty;
+            Cost = new List<Energy>
+            {
+                new Energy(EnergyTypes.Psychic, 1)
+            };
+        }
+
         public override int GetDamage(Player owner, Player opponent)
         {
             return 10;
--- TCGCards/PokemonCards/TeamRocket/Attacks/Vanish.cs
+++ TCGCards/PokemonCards/TeamRocket/Attacks/Vanish.cs
@@ -1,9 +1,20 @@
-﻿using TCGCards.Core;
+﻿using System.Collections.Generic;
+using TCGCards.Core;
 
 namespace TCGCards.PokemonCards.TeamRocket.Attacks
 {
     public class Vanish : Attack
     {
+        public Vanish()
+        {
+            Name = "Vanish";
+            Description = string.Empty;
+            Cost = new List<Energy>
+            {
+                new Energy(EnergyTypes.Psychic, 1)
+            };
+        }
+
         public override int GetDamage(Player owner, Player opponent)
         {
             return 0;
