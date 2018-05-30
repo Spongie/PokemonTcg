229b02852033afd454643758a8d91415a8182eaf
Philip Persson <kalllez123@hotmail.se>
2018-05-30 18:55:04 +0200

    Team Rocket - Rockets sneak attack

---
+++ TCGCards/Core/Messages/PickFromListMessage.cs
@@ -0,0 +1,19 @@
+﻿using NetworkingCore;
+using System.Collections.Generic;
+
+namespace TCGCards.Core.Messages
+{
+    public class PickFromListMessage : AbstractNetworkMessage
+    {
+        public PickFromListMessage(List<ICard> possibleChoices, List<IDeckFilter> filter, int cardCount)
+        {
+            PossibleChoices = possibleChoices;
+            Filter = filter;
+            CardCount = cardCount;
+        }
+
+        public List<ICard> PossibleChoices { get; }
+        public List<IDeckFilter> Filter { get; }
+        public int CardCount { get; }
+    }
+}
