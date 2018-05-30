13d9eada6517bc8592449d095bd9237ba1a12bd0
Philip Persson <kalllez123@hotmail.se>
2018-05-30 18:11:28 +0200

    Team Rocket - Squirtle

---
+++ TeamRocket/Attacks/ShellAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using TCGCards;
+using TCGCards.Core;
+
+namespace TeamRocket.Attacks
+{
+    internal class ShellAttack : Attack
+    {
+        public ShellAttack()
+        {
+            Cost = new List<Energy>
+            {
+                new Energy(EnergyTypes.Colorless, 2)
+            };
+            Name = "Shell attack";
+        }
+
+        public override int GetDamage(Player owner, Player opponent)
+        {
+            return 20;
+        }
+    }
+}
