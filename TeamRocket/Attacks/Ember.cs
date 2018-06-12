836c4f79fc4d2b6118fd8629708eae31b5bce357
Philip Persson <kalllez123@hotmail.se>
2018-06-12 19:42:33 +0200

    Team rocket: Ponyta

---
+++ TeamRocket/Attacks/Ember.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using TCGCards;
+using TCGCards.Core;
+
+namespace TeamRocket.Attacks
+{
+    internal class Ember : Attack
+    {
+        public override int GetDamage(Player owner, Player opponent)
+        {
+            return 30;
+        }
+
+        public override void ProcessEffects(GameField game, Player owner, Player opponent)
+        {
+            var fireEnergy = owner.ActivePokemonCard.AttachedEnergy.First(energy => energy.EnergyType == EnergyTypes.Fire);
+            owner.ActivePokemonCard.AttachedEnergy.Remove(fireEnergy);
+            owner.DiscardPile.Add(fireEnergy);
+        }
+    }
+}
