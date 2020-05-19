4f51b39c39aeedbe688517ad77ec0442f5b606cb
Philip Persson <philipp.dev@hotmail.com>
2020-05-19 22:51:08 +0200

    Scrolling cards in hand

--- PokemonTCGClient/Assets/Code/2D/HandController.cs
+++ PokemonTCGClient/Assets/Code/2D/HandController.cs
@@ -8,20 +8,21 @@ namespace Assets.Code._2D
     public class HandController : MonoBehaviour
     {
         public GameObject cardPrefab;
+        public GameObject cardZone;
         public bool isPlayerHand;
 
         public void SetHand(IEnumerable<Card> cards)
         {
-            for (int i = 0; i < transform.childCount; i++)
+            for (int i = 0; i < cardZone.transform.childCount; i++)
             {
-                Destroy(transform.GetChild(i).gameObject);
+                Destroy(cardZone.transform.GetChild(i).gameObject);
             }
 
             int index = 0;
             foreach (var card in cards)
             {
                 card.IsRevealed = isPlayerHand;
-                var spawnedCard = Instantiate(cardPrefab, transform);
+                var spawnedCard = Instantiate(cardPrefab, cardZone.transform);
                 spawnedCard.GetComponentInChildren<CardRenderer>().SetCard(card, ZoomMode.FromBottom);
                 index++;
             }
@@ -29,9 +30,9 @@ namespace Assets.Code._2D
 
         public void FadeOutCards(IEnumerable<Card> cards)
         {
-            for (int i = 0; i < transform.childCount; i++)
+            for (int i = 0; i < cardZone.transform.childCount; i++)
             {
-                var cardRenderer = transform.GetChild(i).GetComponentInChildren<CardRenderer>();
+                var cardRenderer = cardZone.transform.GetChild(i).GetComponentInChildren<CardRenderer>();
                 CanvasGroup cardCanvas = cardRenderer.GetComponent<CanvasGroup>();
                 cardCanvas.alpha = cards.Any(card => card.Id.Equals(cardRenderer.card.Id)) ? 0.2f : 1f;
 
