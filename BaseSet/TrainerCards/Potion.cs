abe99023d81bc404a2e07213928fc99c6323e6f3
Philip Persson <philipp.dev@hotmail.com>
2020-06-14 19:09:02 +0200

    Potion

---
+++ BaseSet/TrainerCards/Potion.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using TCGCards;
+using TCGCards.Core;
+using TCGCards.Core.Messages;
+
+namespace BaseSet.TrainerCards
+{
+    public class Potion : TrainerCard
+    {
+        public Potion(Player owner) : base(owner)
+        {
+            Name = "Potion";
+            Description = "Remove up to 2 damage counters from 1 of your Pokémon.";
+        }
+
+        public override void Process(GameField game, Player caster, Player opponent)
+        {
+            var response = caster.NetworkPlayer.SendAndWaitForResponse<CardListMessage>(new SelectFromYourPokemonMessage("Select pokemon to heal 20").ToNetworkMessage(game.Id));
+
+            var pokemon = (PokemonCard)game.FindCardById(response.Cards.First());
+
+            pokemon.DamageCounters -= 20;
+
+            if (pokemon.DamageCounters < 0)
+            {
+                pokemon.DamageCounters = 0;
+            }
+        }
+    }
+}
