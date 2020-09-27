4037a0dc3c3898c19eba362ec69aab4cafd03a1a
Philip Persson <philipp.dev@hotmail.com>
2020-09-27 16:29:07 +0200

    Updated buildscript to build client

---
+++ PokemonTCGClient/Assets/Editor/Builder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Builder
+{
+    static void PerformBuild()
+    {
+        Debug.Log("### BUILDING ###");
+
+        var report = BuildPipeline.BuildPlayer(
+            new[] 
+            { 
+                "Assets/Scenes/Login Scene.unity",
+                "Assets/Scenes/UI_2D.unity",
+                "Assets/Scenes/MainMenu.unity",
+                "Assets/Scenes/DeckBuilder.unity"
+            },
+            @"E:\PokemonBuild\Client\PokemonTCGClient.exe",
+            BuildTarget.StandaloneWindows64,
+            BuildOptions.None);
+
+        Debug.Log("###   DONE   ###");
+
+        Debug.Log(report);
+    }
+}
