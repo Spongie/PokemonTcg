7bb17c6c644bc6cf81539de511881f45b772bf42
Philip Persson <philipp.dev@hotmail.com>
2020-10-20 23:30:55 +0200

    Self damage attack

---
+++ TCGCards/Attacks/SelfDamageAttack.cs
@@ -0,0 +1,31 @@
+﻿using CardEditor.Views;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class SelfDamageAttack : Attack
+    {
+        private int amount;
+
+        public SelfDamageAttack() :base()
+        {
+            Name = "Self damage attack";
+        }
+
+        [DynamicInput("SelfDamage")]
+        public int Amount
+        {
+            get { return amount; }
+            set 
+            { 
+                amount = value;
+                FirePropertyChanged();
+            }
+        }
+
+        public override void ProcessEffects(GameField game, Player owner, Player opponent)
+        {
+            owner.ActivePokemonCard.DamageCounters += amount;
+        }
+    }
+}
