0f860d981d4ff5f3ac2c89efee757767669a8757
Philip Persson <philipp.dev@hotmail.com>
2020-10-20 22:54:34 +0200

    Added card draw attack

---
+++ TCGCards/Attacks/DrawCardsAttack.cs
@@ -0,0 +1,26 @@
+﻿using CardEditor.Views;
+
+namespace TCGCards.Attacks
+{
+    public class DrawCardsAttack : Attack
+    {
+        private int amount;
+        
+        public DrawCardsAttack()
+        {
+            Name = "Draw cards";
+        }
+
+        [DynamicInput("Cards to draw")]
+        public int Amount
+        {
+            get { return amount; }
+            set 
+            { 
+                amount = value;
+                FirePropertyChanged();
+            }
+        }
+
+    }
+}
