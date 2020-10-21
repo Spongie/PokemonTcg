f0ed36bf2c180a443eaac3b8f10f3da60eff857b
Philip Persson <philipp.dev@hotmail.com>
2020-10-21 19:12:02 +0200

    Force opponent swap active attack

--- TCGCards/Attacks/OpponentSwitchesActivePokemonAttack.cs
+++ TCGCards/Attacks/OpponentSwitchesActivePokemonAttack.cs
@@ -4,14 +4,14 @@ using TCGCards.Core.Messages;
 
 namespace TCGCards.Attacks
 {
-    public class SwitchActivePokemonAttack : Attack
+    public class OpponentSwitchesActivePokemonAttack : Attack
     {
         public override void ProcessEffects(GameField game, Player owner, Player opponent)
         {
             if (!opponent.BenchedPokemon.Any())
                 return;
 
-            var response = owner.NetworkPlayer.SendAndWaitForResponse<CardListMessage>(new SelectFromOpponentBenchMessage(1).ToNetworkMessage(opponent.Id));
+            var response = opponent.NetworkPlayer.SendAndWaitForResponse<CardListMessage>(new SelectFromOpponentBenchMessage(1).ToNetworkMessage(opponent.Id));
             var newActive = (PokemonCard)game.FindCardById(response.Cards.First());
             opponent.ForceRetreatActivePokemon(newActive);
         }
