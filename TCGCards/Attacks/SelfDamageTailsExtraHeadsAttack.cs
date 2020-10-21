592bd973920e4ad63f5abb037e4bf7c4f5af0338
Philip Persson <philipp.dev@hotmail.com>
2020-10-21 19:22:22 +0200

    Attack for damaging self on tails and extra on heads

---
+++ TCGCards/Attacks/SelfDamageTailsExtraHeadsAttack.cs
@@ -0,0 +1,50 @@
+﻿using CardEditor.Views;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class SelfDamageTailsExtraHeadsAttack : Attack
+    {
+        private int selfDamage;
+        private int extraDamage;
+
+        [DynamicInput("SelfDamage when tails")]
+        public int SelfDamage
+        {
+            get { return selfDamage; }
+            set
+            {
+                selfDamage = value;
+                FirePropertyChanged();
+            }
+        }
+
+        [DynamicInput("Extra Damage when heads")]
+        public int ExtraDamage
+        {
+            get { return extraDamage; }
+            set
+            {
+                extraDamage = value;
+                FirePropertyChanged();
+            }
+        }
+
+        public override Damage GetDamage(Player owner, Player opponent, GameField game)
+        {
+            if (game.FlipCoins(1) == 0)
+            {
+                owner.ActivePokemonCard.DamageCounters += SelfDamage;
+                return Damage;
+            }
+            else
+            {
+                return Damage + ExtraDamage;
+            }
+        }
+    }
+}
