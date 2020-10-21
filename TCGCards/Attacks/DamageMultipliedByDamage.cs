9ed74afd7352c8645ce8bc882ae99b9e163151a9
Philip Persson <philipp.dev@hotmail.com>
2020-10-21 19:32:09 +0200

    Damage multiplied by current damage attack

---
+++ TCGCards/Attacks/DamageMultipliedByDamage.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class DamageMultipliedByDamage : Attack
+    {
+        public override Damage GetDamage(Player owner, Player opponent, GameField game)
+        {
+            return Damage * (owner.ActivePokemonCard.DamageCounters / 10);
+        }
+    }
+}
