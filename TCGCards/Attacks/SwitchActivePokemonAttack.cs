91754429244067e38d6561c9a291950c5329bba9
Philip Persson <philipp.dev@hotmail.com>
2020-10-21 17:54:51 +0200

    Attack for switching active pokemon

---
+++ TCGCards/Attacks/SwitchActivePokemonAttack.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using TCGCards.Core;
+using TCGCards.Core.Messages;
+
+namespace TCGCards.Attacks
+{
+    public class SwitchActivePokemonAttack : Attack
+    {
+        public override void ProcessEffects(GameField game, Player owner, Player opponent)
+        {
+            if (!opponent.BenchedPokemon.Any())
+                return;
+
+            var response = opponent.NetworkPlayer.SendAndWaitForResponse<CardListMessage>(new SelectFromOpponentBenchMessage(1).ToNetworkMessage(opponent.Id));
+            var newActive = (PokemonCard)game.FindCardById(response.Cards.First());
+            opponent.ForceRetreatActivePokemon(newActive);
+        }
+    }
+}
