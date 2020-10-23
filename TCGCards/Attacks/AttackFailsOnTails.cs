4b9ffcdb7c3aa4faaecebc3455b78881ce5cef04
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 19:53:07 +0200

    Failing attacks

---
+++ TCGCards/Attacks/AttackFailsOnTails.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class AttackFailsOnTails : Attack
+    {
+        public override Damage GetDamage(Player owner, Player opponent, GameField game)
+        {
+            if (game.FlipCoins(1) == 0)
+            {
+                game.GameLog.AddMessage("The attack did nothing");
+                return 0;
+            }
+
+            return base.GetDamage(owner, opponent, game);
+        }
+    }
+}
