d6d86716db43886006c10d9c7fdcf25c56a928dc
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 19:50:18 +0200

    Suicide style attacks

---
+++ TCGCards/Attacks/DamageManyAttack.cs
@@ -0,0 +1,58 @@
+﻿using CardEditor.Views;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class DamageManyAttack : Attack
+    {
+        private int selfDamage;
+        private int teamBenchDamage;
+        private int enemyBenchDamage;
+
+        [DynamicInput("Damage to your bench")]
+        public int TeamBenchDamage
+        {
+            get { return teamBenchDamage; }
+            set
+            {
+                teamBenchDamage = value;
+                FirePropertyChanged();
+            }
+        }
+
+        [DynamicInput("Damage to opponents bench")]
+        public int EnemyBenchDamage
+        {
+            get { return enemyBenchDamage; }
+            set
+            {
+                enemyBenchDamage = value;
+                FirePropertyChanged();
+            }
+        }
+
+        [DynamicInput("Damage to self")]
+        public int SelfDamage
+        {
+            get { return selfDamage; }
+            set 
+            { 
+                selfDamage = value;
+                FirePropertyChanged();
+            }
+        }
+
+        public override void ProcessEffects(GameField game, Player owner, Player opponent)
+        {
+            owner.ActivePokemonCard.DamageCounters += SelfDamage;
+            foreach (var pokemon in opponent.BenchedPokemon)
+            {
+                pokemon.DamageCounters += EnemyBenchDamage;
+            }
+            foreach (var pokemon in owner.BenchedPokemon)
+            {
+                pokemon.DamageCounters += TeamBenchDamage;
+            }
+        }
+    }
+}
