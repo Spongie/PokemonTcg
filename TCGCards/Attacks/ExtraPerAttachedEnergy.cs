ab3ef609ff28a7cac655cc6bdf5d557ee4d10721
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 21:55:00 +0200

    Extra damage per attached energy to user

---
+++ TCGCards/Attacks/ExtraPerAttachedEnergy.cs
@@ -0,0 +1,28 @@
+﻿using CardEditor.Views;
+using Entities;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class ExtraPerAttachedEnergy : Attack
+    {
+        private int extraPerEnergy;
+
+        [DynamicInput("Extra per damage counter")]
+        public int ExtraPerEnergy
+        {
+            get { return extraPerEnergy; }
+            set
+            {
+                extraPerEnergy = value;
+                FirePropertyChanged();
+            }
+        }
+
+        public override Damage GetDamage(Player owner, Player opponent, GameField game)
+        {
+            var extraDamage = owner.ActivePokemonCard.AttachedEnergy.Count * ExtraPerEnergy;
+            return Damage + extraDamage;
+        }
+    }
+}
