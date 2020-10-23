0dbf63a2fbf2ea3865615048084e84243cf65ccc
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 21:51:42 +0200

    Extra damage based on damage on target (Meditate from Jynx)

---
+++ TCGCards/Attacks/ExtraPerDamageOnTarget.cs
@@ -0,0 +1,28 @@
+﻿using CardEditor.Views;
+using Entities;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class ExtraPerDamageOnTarget : Attack
+    {
+        private int extraPerDamageCounter;
+
+        [DynamicInput("Extra per damage counter")]
+        public int ExtraPerDamageCounter
+        {
+            get { return extraPerDamageCounter; }
+            set 
+            { 
+                extraPerDamageCounter = value;
+                FirePropertyChanged();
+            }
+        }
+
+        public override Damage GetDamage(Player owner, Player opponent, GameField game)
+        {
+            var extraDamage = (opponent.ActivePokemonCard.DamageCounters / 10) * extraPerDamageCounter;
+            return Damage + extraDamage;
+        }
+    }
+}
