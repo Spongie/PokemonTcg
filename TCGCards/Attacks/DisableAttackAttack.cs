3bf3a150168f468e91e6bc9349bf7327e1f527ef
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 20:03:06 +0200

    Disable attack attack

---
+++ TCGCards/Attacks/DisableAttackAttack.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using TCGCards.Core;
+using TCGCards.Core.Messages;
+
+namespace TCGCards.Attacks
+{
+    public class DisableAttackAttack : Attack
+    {
+        public override void ProcessEffects(GameField game, Player owner, Player opponent)
+        {
+            if (opponent.ActivePokemonCard.Attacks.Count == 1)
+            {
+                opponent.ActivePokemonCard.Attacks[0].Disabled = true;
+                game.GameLog.AddMessage($"{owner.NetworkPlayer?.Name} disables attack {opponent.ActivePokemonCard.Attacks[0].Name}");
+                return;
+            }
+
+            var attackMessage = new SelectAttackMessage(opponent.ActivePokemonCard.Attacks).ToNetworkMessage(owner.Id);
+            var chosenAttack = owner.NetworkPlayer.SendAndWaitForResponse<AttackMessage>(attackMessage).Attack;
+
+            game.GameLog.AddMessage($"{owner.NetworkPlayer?.Name} disables attack {chosenAttack.Name}");
+            opponent.ActivePokemonCard.Attacks.First(x => x.Id.Equals(chosenAttack.Id)).Disabled = true;
+        }
+    }
+}
