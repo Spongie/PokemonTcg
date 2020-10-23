42cecc62d07079b74f475184da704931a263a850
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 21:14:32 +0200

    Attack apply attack prevention effect

---
+++ TCGCards/Attacks/ApplyAttackPrevention.cs
@@ -0,0 +1,32 @@
+﻿using CardEditor.Views;
+using TCGCards.Core;
+using TCGCards.Core.SpecialAbilities;
+
+namespace TCGCards.Attacks
+{
+    public class ApplyAttackPrevention : Attack
+    {
+        private bool coinFlip;
+
+        [DynamicInput("Coin flipped", InputControl.Boolean)]
+        public bool CoinFlip
+        {
+            get { return coinFlip; }
+            set
+            {
+                coinFlip = value;
+                FirePropertyChanged();
+            }
+        }
+
+        public override void ProcessEffects(GameField game, Player owner, Player opponent)
+        {
+            if (CoinFlip && game.FlipCoins(1) == 0)
+            {
+                return;
+            }
+
+            owner.ActivePokemonCard.AttackStoppers.Add(new AttackStopper((x) => x == owner.ActivePokemonCard ));
+        }
+    }
+}
