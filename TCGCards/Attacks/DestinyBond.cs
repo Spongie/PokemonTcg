ccdde897da17c9ab891e2d9277744ee13d2fa6d8
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 19:31:24 +0200

    Created destiny bond attack

---
+++ TCGCards/Attacks/DestinyBond.cs
@@ -0,0 +1,31 @@
+﻿using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class DestinyBond : Attack
+    {
+        public DestinyBond() :base()
+        {
+            Name = "Destiny Bond";
+        }
+
+        public override void ProcessEffects(GameField game, Player owner, Player opponent)
+        {
+            owner.ActivePokemonCard.TemporaryAbilities.Add(new DestinyBondAbilty(owner.ActivePokemonCard));
+        }
+
+        private class DestinyBondAbilty : TemporaryAbility
+        {
+            public DestinyBondAbilty(PokemonCard owner) : base(owner)
+            {
+                TriggerType = TriggerType.Dies;
+            }
+
+            protected override void Activate(Player owner, Player opponent, int damageTaken, GameLog log)
+            {
+                log.AddMessage($"{PokemonOwner.KnockedOutBy.GetName()} knocked out {PokemonOwner.Name} and dies itself");
+                PokemonOwner.KnockedOutBy.DamageCounters = 9000;
+            }
+        }
+    }
+}
