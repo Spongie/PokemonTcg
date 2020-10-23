1b5d8ddc918c09691a0c1dd49883d07f36994928
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 21:38:04 +0200

    Mirror Move

---
+++ TCGCards/Attacks/MirrorMove.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class MirrorMove : Attack
+    {
+        public override Damage GetDamage(Player owner, Player opponent, GameField game)
+        {
+            return owner.ActivePokemonCard.DamageTakenLastTurn;
+        }
+    }
+}
