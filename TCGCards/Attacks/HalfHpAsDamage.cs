6a7665f6f6ebcfe239108d19adb0f18c7779afcc
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 21:37:06 +0200

    Deal half HP as damage

---
+++ TCGCards/Attacks/HalfHpAsDamage.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class HalfHpAsDamage : Attack
+    {
+        public override Damage GetDamage(Player owner, Player opponent, GameField game)
+        {
+            var damage = (opponent.ActivePokemonCard.Hp - opponent.ActivePokemonCard.DamageCounters) / 2;
+
+            if (damage.ToString().EndsWith("5"))
+                damage += 5;
+
+            return damage;
+        }
+    }
+}
