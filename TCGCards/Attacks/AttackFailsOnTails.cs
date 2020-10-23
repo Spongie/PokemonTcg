fe80e52c83effc6e1e36f52085cd33329b3f15b5
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 21:10:19 +0200

    Attack fails forever (leek slap)

--- TCGCards/Attacks/AttackFailsOnTails.cs
+++ TCGCards/Attacks/AttackFailsOnTails.cs
@@ -1,10 +1,24 @@
-﻿using Entities;
+﻿using CardEditor.Views;
+using Entities;
 using TCGCards.Core;
 
 namespace TCGCards.Attacks
 {
     public class AttackFailsOnTails : Attack
     {
+        private bool isOneTime;
+
+        [DynamicInput("Fails forever?", InputControl.Boolean)]
+        public bool IsOneTimeUse
+        {
+            get { return isOneTime; }
+            set 
+            { 
+                isOneTime = value;
+                FirePropertyChanged();
+            }
+        }
+
         public override Damage GetDamage(Player owner, Player opponent, GameField game)
         {
             if (game.FlipCoins(1) == 0)
