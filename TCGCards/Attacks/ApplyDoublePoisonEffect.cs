1690c9fe7f3f8d1e9c294c8e6a7daedadba45b82
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 20:59:48 +0200

    Apply Double Poison Effect

---
+++ TCGCards/Attacks/ApplyDoublePoisonEffect.cs
@@ -0,0 +1,13 @@
+﻿using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class ApplyDoublePoisonEffect : Attack
+    {
+        public override void ProcessEffects(GameField game, Player owner, Player opponent)
+        {
+            opponent.ActivePokemonCard.IsPoisoned = true;
+            opponent.ActivePokemonCard.DoublePoison = true;
+        }
+    }
+}
