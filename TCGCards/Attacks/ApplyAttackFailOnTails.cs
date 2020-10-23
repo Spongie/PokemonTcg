f70bd06fa9b0a29574f40c74593f795da57fc35b
Philip Persson <philipp.dev@hotmail.com>
2020-10-23 21:33:49 +0200

    ApplyAttackFailOnTails

---
+++ TCGCards/Attacks/ApplyAttackFailOnTails.cs
@@ -0,0 +1,17 @@
+﻿using Entities;
+using TCGCards.Core;
+using TCGCards.Core.SpecialAbilities;
+
+namespace TCGCards.Attacks
+{
+    public class ApplyAttackFailOnTails : Attack
+    {
+        public override void ProcessEffects(GameField game, Player owner, Player opponent)
+        {
+            opponent.ActivePokemonCard.AttackStoppers.Add(new AttackStopper((x) =>
+            {
+                return CoinFlipper.FlipCoin() == CoinFlipper.TAILS;
+            }));
+        }
+    }
+}
