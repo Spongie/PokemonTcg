f534703bc7dc9607c498a9e9c5d9a3b8d7f50180
Philip Persson <philipp.dev@hotmail.com>
2020-11-01 16:54:25 +0100

    Added easy clone function

---
+++ TCGCards/Core/SystemExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace TCGCards.Core
+{
+    public static class SystemExtensions
+    {
+        public static T Clone<T>(this T source)
+        {
+            var serialized = JsonConvert.SerializeObject(source);
+            return JsonConvert.DeserializeObject<T>(serialized);
+        }
+    }
+}
