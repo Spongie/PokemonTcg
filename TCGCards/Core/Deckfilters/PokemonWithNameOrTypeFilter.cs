e61f5abbff0fcfd323196dd38a705af1f59058ea
Philip Persson <philipp.dev@hotmail.com>
2020-11-12 21:42:26 +0100

    Search deck for pokemon effects

---
+++ TCGCards/Core/Deckfilters/PokemonWithNameOrTypeFilter.cs
@@ -0,0 +1,45 @@
+﻿using Entities;
+using System.Linq;
+
+namespace TCGCards.Core.Deckfilters
+{
+    public class PokemonWithNameOrTypeFilter : IDeckFilter
+    {
+        public PokemonWithNameOrTypeFilter()
+        {
+
+        }
+
+        public PokemonWithNameOrTypeFilter(string names, EnergyTypes type)
+        {
+            Names = names;
+            Type = type;
+        }
+
+        public string Names { get; set; }
+        public EnergyTypes Type { get; set; }
+
+        public bool IsCardValid(Card card)
+        {
+            bool valid = false;
+            var pokemon = card as PokemonCard;
+
+            if (pokemon == null)
+            {
+                return false;
+            }
+
+            if (Type != EnergyTypes.All && Type != EnergyTypes.None)
+            {
+                valid = pokemon.Type == Type;
+            }
+            
+            if (valid && !string.IsNullOrEmpty(Names))
+            {
+                valid = Names.Split(';').Contains(card.Name);
+            }
+
+            return valid;
+        }
+    }
+}
---
+++ TCGCards/TrainerEffects/SearchDeckForPokemon.cs
@@ -0,0 +1,112 @@
+﻿using CardEditor.Views;
+using Entities;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TCGCards.Core;
+using TCGCards.Core.Deckfilters;
+using TCGCards.Core.Messages;
+
+namespace TCGCards.TrainerEffects
+{
+    public class SearchDeckForPokemon : DataModel, IEffect
+    {
+        public string EffectType
+        {
+            get
+            {
+                return "Search your deck for Pokémon";
+            }
+        }
+
+        private bool addToBench;
+        private EnergyTypes energyType = EnergyTypes.None;
+        private string names;
+        private int amount;
+
+        [DynamicInput("Amount")]
+        public int Amount
+        {
+            get { return amount; }
+            set
+            {
+                amount = value;
+                FirePropertyChanged();
+            }
+        }
+
+        [DynamicInput("Names to search for (Split with ;)")]
+        public string Names
+        {
+            get { return names; }
+            set
+            {
+                names = value;
+                FirePropertyChanged();
+            }
+        }
+
+
+        [DynamicInput("Only of type", InputControl.Dropdown, typeof(EnergyTypes))]
+        public EnergyTypes EnergyType
+        {
+            get { return energyType; }
+            set
+            {
+                energyType = value;
+                FirePropertyChanged();
+            }
+        }
+
+
+        [DynamicInput("Add to bench")]
+        public bool AddToBench
+        {
+            get { return addToBench; }
+            set
+            {
+                addToBench = value;
+                FirePropertyChanged();
+            }
+        }
+
+
+        public bool CanCast(GameField game, Player caster, Player opponent)
+        {
+            if (AddToBench)
+            {
+                return GameField.BenchMaxSize - caster.BenchedPokemon.Count - amount >= 0;
+            }
+
+            return true;
+        }
+
+        public void OnAttachedTo(PokemonCard attachedTo, bool fromHand)
+        {
+            
+        }
+
+        public void Process(GameField game, Player caster, Player opponent)
+        {
+            var message = new DeckSearchMessage(caster.Deck, new List<IDeckFilter> { new PokemonWithNameOrTypeFilter(Names, EnergyType) }, Amount);
+            var response = caster.NetworkPlayer.SendAndWaitForResponse<CardListMessage>(message.ToNetworkMessage(caster.Id));
+
+
+            foreach (var card in response.Cards.Select(id => game.FindCardById(id)))
+            {
+                caster.Deck.Cards = new Stack<Card>(caster.Deck.Cards.Except(new[] { card }));
+                
+                if (addToBench)
+                {
+                    caster.BenchedPokemon.Add((PokemonCard)card);
+                }
+                else
+                {
+                    caster.Hand.Add(card);
+                }
+            }
+        }
+    }
+}
