247f436ca1e40bad0152d751ece952316d1fb49b
Philip Persson <philipp.dev@hotmail.com>
2020-11-13 21:17:15 +0100

    Flip coin attack for each attached energy

---
+++ TCGCards/Attacks/FlipCoinForeachEnergy.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class FlipCoinForeachEnergy : Attack
+    {
+        public override Damage GetDamage(Player owner, Player opponent, GameField game)
+        {
+            return game.FlipCoins(owner.ActivePokemonCard.AttachedEnergy.Count) * Damage;
+        }
+    }
+}
