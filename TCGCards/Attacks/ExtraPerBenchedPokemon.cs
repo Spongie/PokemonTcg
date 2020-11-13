028359b69e8246a500d464ef9113764c7f8d119e
Philip Persson <philipp.dev@hotmail.com>
2020-11-13 20:39:40 +0100

    Extra damage per benched pokemon

---
+++ TCGCards/Attacks/ExtraPerBenchedPokemon.cs
@@ -0,0 +1,27 @@
+﻿using CardEditor.Views;
+using Entities;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class ExtraPerBenchedPokemon : Attack
+    {
+        private int extra;
+
+        [DynamicInput("Extra damage")]
+        public int ExtraPerPokemon
+        {
+            get { return extra; }
+            set
+            {
+                extra = value;
+                FirePropertyChanged();
+            }
+        }
+
+        public override Damage GetDamage(Player owner, Player opponent, GameField game)
+        {
+            return Damage + ExtraPerPokemon * owner.BenchedPokemon.Count;
+        }
+    }
+}
