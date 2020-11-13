0bda44af1c8a1ac5d47a88314317133a37c07f7b
Philip Persson <philipp.dev@hotmail.com>
2020-11-13 20:38:26 +0100

    Flip a coin for each benched pokemon attack

---
+++ TCGCards/Attacks/FlipCoinForeachBenched.cs
@@ -0,0 +1,13 @@
+﻿using Entities;
+using TCGCards.Core;
+
+namespace TCGCards.Attacks
+{
+    public class FlipCoinForeachBenched : Attack
+    {
+        public override Damage GetDamage(Player owner, Player opponent, GameField game)
+        {
+            return game.FlipCoins(owner.BenchedPokemon.Count) * Damage;
+        }
+    }
+}
