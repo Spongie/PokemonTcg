d21ff3c076ba163e9ca9896fffb42581b9735a60
Philip Persson <philipp.dev@hotmail.com>
2020-11-14 18:33:19 +0100

    Heal and discard all energy (Pokemon Center)

---
+++ TCGCards/TrainerEffects/HealAndDiscardAllEnergy.cs
@@ -0,0 +1,42 @@
+﻿using Entities.Models;
+using TCGCards.Core;
+
+namespace TCGCards.TrainerEffects
+{
+    public class HealAndDiscardAllEnergy : DataModel, IEffect
+    {
+        public string EffectType
+        {
+            get
+            {
+                return "Heal and Discard all attached energy";
+            }
+        }
+
+        public bool CanCast(GameField game, Player caster, Player opponent)
+        {
+            return true;
+        }
+
+        public void OnAttachedTo(PokemonCard attachedTo, bool fromHand)
+        {
+            
+        }
+
+        public void Process(GameField game, Player caster, Player opponent)
+        {
+            foreach (var pokemon in caster.GetAllPokemonCards())
+            {
+                if (pokemon.DamageCounters == 0)
+                {
+                    continue;
+                }
+
+                pokemon.DamageCounters = 0;
+
+                caster.DiscardPile.AddRange(pokemon.AttachedEnergy);
+                pokemon.AttachedEnergy.Clear();
+            }
+        }
+    }
+}
