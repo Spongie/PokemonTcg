9d12d36a10b6afd5e1869326113e6d030e219baf
Philip Persson <philipp.dev@hotmail.com>
2020-11-18 23:43:31 +0100

    Deleted old unused code

--- DataLayer/Queries/AndFilter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace DataLayer.Queries
-{
-    internal class AndFilter : Filter
-    {
-        public override string GenerateSql() => $"{Column.Name} = {Util.GetValueSqlFormatted(Value, Column.PropertyType)}";
-    }
-}
