0dbf6bb29c50e21f85d07092d65549524f196248
Philip Persson <philipp.dev@hotmail.com>
2020-11-29 20:50:12 +0100

    Ability to swap prize with top of deck (Trickery)

---
+++ TCGCards/Core/Abilities/SwapPrizeWithTopOfDeck.cs
@@ -0,0 +1,35 @@
+﻿using NetworkingCore;
+using System.Linq;
+using TCGCards.Core.Messages;
+
+namespace TCGCards.Core.Abilities
+{
+    public class SwapPrizeWithTopOfDeck : Ability
+    {
+        public SwapPrizeWithTopOfDeck() :this(null)
+        {
+
+        }
+
+        public SwapPrizeWithTopOfDeck(PokemonCard owner) :base(owner)
+        {
+
+        }
+
+        protected override void Activate(Player owner, Player opponent, int damageTaken, GameField game)
+        {
+            var message = new SelectPriceCardsMessage(1).ToNetworkMessage(NetworkId.Generate());
+            var response = owner.NetworkPlayer.SendAndWaitForResponse<CardListMessage>(message);
+
+            var selectedCard = game.FindCardById(response.Cards.First());
+
+            owner.PrizeCards.Remove(selectedCard);
+
+            var topOfDeck = owner.Deck.Cards.Pop();
+
+            owner.PrizeCards.Add(topOfDeck);
+
+            owner.Deck.Cards.Push(selectedCard);
+        }
+    }
+}
