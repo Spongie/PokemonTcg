5eaa5aa7ff5f3af7cee719f5734d7c6bd67c66f8
Philip Persson <philipp.dev@hotmail.com>
2020-11-30 22:24:27 +0100

    Don't retreat self and only if you actually have a replacement

--- PokemonTCGClient/Assets/Code/UI/Gameplay/CardPopupHandler.cs
+++ PokemonTCGClient/Assets/Code/UI/Gameplay/CardPopupHandler.cs
@@ -39,7 +39,7 @@ namespace Assets.Code.UI.Gameplay
                         attackButtons.Add(attackButton);
                     }
 
-                    RetreatButton.SetActive(pokemonCard.AttachedEnergy.Sum(energy => energy.GetEnergry().Amount) >= pokemonCard.RetreatCost);
+                    RetreatButton.SetActive(pokemonCard.AttachedEnergy.Sum(energy => energy.GetEnergry().Amount) >= pokemonCard.RetreatCost && player.BenchedPokemon.Count > 0);
                 }
                 else
                 {
