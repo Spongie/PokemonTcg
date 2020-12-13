b6747dade184884e4b42a054e3d8d7ec5b236259
Philip Persson <philipp.dev@hotmail.com>
2020-12-13 11:18:46 +0100

    Reveal prize cards effect

---
+++ TCGCards/TrainerEffects/RevealPrizeCardsEffect.cs
@@ -0,0 +1,38 @@
+﻿using TCGCards.Core;
+
+namespace TCGCards.TrainerEffects
+{
+    public class RevealPrizeCardsEffect : IEffect
+    {
+        public string EffectType
+        {
+            get
+            {
+                return "Reveal prize cards";
+            }
+        }
+
+        public bool CanCast(GameField game, Player caster, Player opponent)
+        {
+            return true;
+        }
+
+        public void OnAttachedTo(PokemonCard attachedTo, bool fromHand, GameField game)
+        {
+            
+        }
+
+        public void Process(GameField game, Player caster, Player opponent, PokemonCard pokemonSource)
+        {
+            foreach (var prizeCard in caster.PrizeCards)
+            {
+                prizeCard.IsRevealed = true;
+            }
+
+            foreach (var prizeCard in opponent.PrizeCards)
+            {
+                prizeCard.IsRevealed = true;
+            }
+        }
+    }
+}
