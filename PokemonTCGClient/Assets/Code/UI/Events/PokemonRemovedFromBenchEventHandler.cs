e185ac40b3135527e57b9bf18e46ef4527301d3f
Philip Persson <philipp.dev@hotmail.com>
2020-12-18 22:37:26 +0100

    removed from bench handler

--- PokemonTCGClient/Assets/Code/UI/Events/PokemonRemovedFromBenchEventHandler.cs
+++ PokemonTCGClient/Assets/Code/UI/Events/PokemonRemovedFromBenchEventHandler.cs
@@ -1,17 +1,32 @@
-﻿using TCGCards.Core.GameEvents;
+﻿using Assets.Code._2D;
+using TCGCards;
+using TCGCards.Core.GameEvents;
 using UnityEngine;
 
 namespace Assets.Code.UI.Events
 {
     public class PokemonRemovedFromBenchEventHandler : MonoBehaviour
     {
+        public BenchController playerBench;
+        public BenchController opponentBench;
+
         public void Trigger(PokemonRemovedFromBench removedFromBenchEvent)
         {
             var pokemon = GameController.Instance.GetCardRendererById(removedFromBenchEvent.PokemonId);
+            var isMyPokemon = pokemon.card.Owner.Id.Equals(GameController.Instance.myId);
 
             pokemon.GetComponent<RectTransform>().LeanAlpha(0, 0.75f).setOnComplete(() =>
             {
-                GameController.Instance.playerHand.RemoveCardById(removedFromBenchEvent.PokemonId);
+                if (isMyPokemon)
+                {
+                    GameController.Instance.Player.BenchedPokemon.Remove((PokemonCard)pokemon.card);
+                }
+                else
+                {
+                    GameController.Instance.OpponentPlayer.BenchedPokemon.Remove((PokemonCard)pokemon.card);
+                }
+                
+                Destroy(pokemon.gameObject);
                 GameEventHandler.Instance.EventCompleted();
             });
         }
