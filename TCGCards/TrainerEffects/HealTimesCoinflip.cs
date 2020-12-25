b6951278ef2fbd0ebbba4dc958603acbf12e9ab5
Philip Persson <philipp.dev@hotmail.com>
2020-12-25 18:53:09 +0100

    Heal multiplied with coins

---
+++ TCGCards/TrainerEffects/HealTimesCoinflip.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CardEditor.Views;
+using Entities.Models;
+using TCGCards.Core;
+using TCGCards.TrainerEffects.Util;
+
+namespace TCGCards.TrainerEffects
+{
+    public class HealTimesCoinflip : DataModel, IEffect
+    {
+        private int coins;
+        private int amount;
+        private TargetingMode targetingMode;
+        
+        public string EffectType
+        {
+            get
+            {
+                return "Heal x Heads";
+            }
+        }
+
+        [DynamicInput("Coins to flip")]
+        public int Coins
+        {
+            get { return coins; }
+            set
+            {
+                coins = value;
+                FirePropertyChanged();
+            }
+        }
+
+        [DynamicInput("Heal per heads")]
+        public int AmountPerCoin
+        {
+            get { return amount; }
+            set
+            {
+                amount = value;
+                FirePropertyChanged();
+            }
+        }
+
+        [DynamicInput("Target", InputControl.Dropdown, typeof(TargetingMode))]
+        public TargetingMode TargetingMode
+        {
+            get { return targetingMode; }
+            set
+            {
+                targetingMode = value;
+                FirePropertyChanged();
+            }
+        }
+
+
+        public bool CanCast(GameField game, Player caster, Player opponent)
+        {
+            return true;
+        }
+
+        public void OnAttachedTo(PokemonCard attachedTo, bool fromHand, GameField game)
+        {
+            
+        }
+
+        public void Process(GameField game, Player caster, Player opponent, PokemonCard pokemonSource)
+        {
+            var target = Targeting.AskForTargetFromTargetingMode(TargetingMode, game, caster, opponent, pokemonSource);
+            int heads = game.FlipCoins(Coins);
+
+            target.Heal(heads * AmountPerCoin, game);
+        }
+    }
+}
