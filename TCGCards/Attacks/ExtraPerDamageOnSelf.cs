bf23a16a3a181ef4e4e8eb525574ef16bc00c409
Philip Persson <philipp.dev@hotmail.com>
2020-12-30 12:32:51 +0100

    Coinflip for extra damage on self

--- TCGCards/Attacks/ExtraPerDamageOnSelf.cs
+++ TCGCards/Attacks/ExtraPerDamageOnSelf.cs
@@ -1,12 +1,25 @@
 ﻿using CardEditor.Views;
 using Entities;
 using TCGCards.Core;
+using TCGCards.TrainerEffects.Util;
 
 namespace TCGCards.Attacks
 {
     public class ExtraPerDamageOnSelf : Attack
     {
         private int extraPerDamageCounter;
+        private CoinFlipConditional coinFlipConditional = new CoinFlipConditional();
+
+        [DynamicInput("Condition", InputControl.Dynamic)]
+        public CoinFlipConditional CoinFlipConditional
+        {
+            get { return coinFlipConditional; }
+            set
+            {
+                coinFlipConditional = value;
+                FirePropertyChanged();
+            }
+        }
 
         [DynamicInput("Extra per damage counter")]
         public int ExtraPerDamageCounter
@@ -21,6 +34,11 @@ namespace TCGCards.Attacks
 
         public override Damage GetDamage(Player owner, Player opponent, GameField game)
         {
+            if (!CoinFlipConditional.IsOk(game, owner))
+            {
+                return 0;
+            }
+
             var extraDamage = (owner.ActivePokemonCard.DamageCounters / 10) * extraPerDamageCounter;
             return Damage + extraDamage;
         }
